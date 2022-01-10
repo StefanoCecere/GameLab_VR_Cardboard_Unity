318847312560affffa6ccc1d8d0131d199d2733c
Stefano Cecere <stefano.cecere@krur.com>
2022-01-10 19:44:35 +0100

    cardboard-xr-plugin v1.11.0

--- Assets/Hello Cardboard/Scripts/CameraPointer.cs
+++ Assets/Hello Cardboard/Scripts/CameraPointer.cs
@@ -35,22 +35,27 @@ public class CameraPointer : MonoBehaviour
         // Casts ray towards camera's forward direction, to detect if a GameObject is being gazed
         // at.
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, _maxDistance)) {
+        if (Physics.Raycast(transform.position, transform.forward, out hit, _maxDistance))
+        {
             // GameObject detected in front of the camera.
-            if (_gazedAtObject != hit.transform.gameObject) {
+            if (_gazedAtObject != hit.transform.gameObject)
+            {
                 // New GameObject.
                 _gazedAtObject?.SendMessage("OnPointerExit");
                 _gazedAtObject = hit.transform.gameObject;
-                _gazedAtObject.SendMessage("OnPointerEnter", SendMessageOptions.DontRequireReceiver);
+                _gazedAtObject.SendMessage("OnPointerEnter");
             }
-        } else {
+        }
+        else
+        {
             // No GameObject detected in front of the camera.
             _gazedAtObject?.SendMessage("OnPointerExit");
             _gazedAtObject = null;
         }
 
         // Checks for screen touches.
-        if (Google.XR.Cardboard.Api.IsTriggerPressed) {
+        if (Google.XR.Cardboard.Api.IsTriggerPressed)
+        {
             _gazedAtObject?.SendMessage("OnPointerClick");
         }
     }
