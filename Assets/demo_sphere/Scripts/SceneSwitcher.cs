4c13b6d18245bfc54ab17b7289d02471350ec170
Stefano Cecere <stefano.cecere@krur.com>
2022-02-22 23:31:29 +0100

    demo sphere

---
+++ Assets/demo_sphere/Scripts/SceneSwitcher.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using Google.XR.Cardboard;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public float TapDelay = 1.0f;
+    float _tapCountdown;
+
+    int _currentSceneIndex = 0;
+
+    static SceneSwitcher _instance;
+    public static SceneSwitcher Instance
+    {
+        get
+        {
+            return _instance;
+        }
+        set { }
+    }
+
+    void Awake()
+    {
+        if (_instance != null)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            _instance = this;
+        }
+
+        _tapCountdown = TapDelay;
+    }
+
+    void Update()
+    {
+        if (Api.IsTriggerPressed)
+        {
+            _tapCountdown = TapDelay;
+
+            _currentSceneIndex++;
+            if (_currentSceneIndex >= SceneManager.sceneCountInBuildSettings) _currentSceneIndex = 0;
+            SceneManager.LoadScene(_currentSceneIndex);
+        }
+        else
+        {
+            _tapCountdown -= Time.deltaTime;
+        }
+    }
+}
